9df5e2b7632ea41c41ca9114dfd15590865026d1
Chetan <rhyme2chetan@gmail.com>
2023-03-24 16:31:59 +0530

    Fix: setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 HERE = os.path.dirname(os.path.abspath(__file__))
 
-GITHUB_URL = "https://github.com/facultyai/dash-material-components/"
+GITHUB_URL = "https://github.com/rhyme2chetan/dash-material-components.git"
 
 
 def _get_version():
@@ -19,26 +19,26 @@ def _get_version():
     return version
 
 
-def _get_long_description():
-    with io.open(os.path.join(HERE, "landing-page.md"), encoding="utf8") as f:
-        return f.read()
+# def _get_long_description():
+#     with io.open(os.path.join(HERE, "landing-page.md"), encoding="utf8") as f:
+#         return f.read()
 
 
 setup(
     name="dash-material-components",
     version=_get_version(),
     description="material themed components for use in Plotly Dash",
-    long_description=_get_long_description(),
+    long_description=_"Dash version of Material UI",
     long_description_content_type="text/markdown",
     license="Apache Software License",
     author="Faculty",
-    author_email="opensource@faculty.ai",
-    url="https://dash-material-components.opensource.faculty.ai/",
+    author_email="rhyme2chetan@gmail.com",
+    url="https://dash-material-components.opensource.ai/",
     project_urls={
         "Bug Reports": os.path.join(GITHUB_URL, "issues"),
         "Source": GITHUB_URL,
     },
-    packages=find_packages(),
+    packages=["dash-material-components"],
     install_requires=["dash>=2.0.0"],
     include_package_data=True,
     classifiers=[
